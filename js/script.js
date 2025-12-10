a6aa31d13f80efbe66a6767a5a17235bb4509215
TamerSoup625 <danielegiunta2007@gmail.com>
2025-12-10 16:35:52 +0100

    Mappa per mobile

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let infoSentieri;
+
+fetch("sentieri.json")
+    .then(response => response.json())
+    .then(obj => {
+        infoSentieri = obj;
+    })
+
+function showPoint(identifier) {
+    console.log(identifier);
+    const sentiero = infoSentieri[identifier];
+    $("#mappa-modale-img").attr("src", "");
+    $("#mappa-modale-img").attr("src", sentiero.img);
+    $("#mappa-modale-titolo").text(sentiero.nome);
+    $("#mappa-modale-link").attr("href", sentiero.link);
+    $("#mappa-modale-descrizione").text(sentiero.descrizione);
+    $("#mappa-modale")[0].showModal();
+}
+
+window.onresize = () => {
+    if (window.matchMedia("screen and (orientation: portrait) and (max-width: 500pt)").matches) {
+        $("#mappa-modale")[0].close();
+    }
+}
